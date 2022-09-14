89c20d166fd5ecde62819b922ac88587843238de
azurinrahim <113412950+azurinrahim@users.noreply.github.com>
2022-09-14 14:02:50 +0800

    Add files via upload

---
+++ mallapp.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.header("Mall App")
+st.write(pd.df({
+    'Intplan': ['yes', 'yes', 'yes', 'no'],
+    'Churn Status': [0, 0, 0, 1]
+}))
